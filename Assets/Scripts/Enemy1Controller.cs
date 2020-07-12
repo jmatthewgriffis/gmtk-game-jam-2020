a3ce5bb4c30e14d2848fad573ee47a6f7dd1fb7a
J. Matthew Griffis <matticgreek@gmail.com>
2020-07-12 10:11:21 -0600

    Create `Enemy 1` with customizable movement range.

---
+++ Assets/Scripts/Enemy1Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Controller : MonoBehaviour
+{
+  public float moveSpeed = 1f;
+  public float fireSpeed = 1f;
+  public Transform spawnPoint;
+  public GameObject bolt;
+
+  private Rigidbody2D rb2D;
+
+  private void MissingComponent(string name)
+  {
+    Debug.LogError("Missing " + name + "!");
+  }
+
+  private Rigidbody2D GetRigidbody2D()
+  {
+    Rigidbody2D component = GetComponent<Rigidbody2D>();
+    if (!component) { MissingComponent("Rigidbody2D"); }
+    return component;
+  }
+
+  void Start()
+  {
+    rb2D = GetRigidbody2D();
+  }
+
+  private void Move()
+  {
+    Vector3 worldOffset3D = transform.TransformDirection(new Vector3(moveSpeed, 0f, 0f));
+    Vector2 worldOffset2D = new Vector2(worldOffset3D.x, worldOffset3D.y);
+    rb2D.MovePosition(rb2D.position + worldOffset2D * Time.fixedDeltaTime);
+  }
+
+  void FixedUpdate()
+  {
+    Move();
+  }
+
+  void OnTriggerExit2D(Collider2D other)
+  {
+    if (other.CompareTag("Range"))
+    {
+      moveSpeed *= -1;
+    }
+  }
+}
